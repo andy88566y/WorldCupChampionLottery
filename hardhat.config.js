34095b7852ca892b3db874394c7b382fea008dbd
楊茂榮 <andy88566y@gmail.com>
2022-12-13 16:13:41 +0100

    add configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+// const GOERLI_ACCOUNT_PRIVATE_KEY = process.env.GOERLI_ACCOUNT_PRIVATE_KEY;
+// const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+// const URL = process.env.URL;
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: {
+		compilers: [
+			{
+				version: "0.8.17",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+					viaIR: true,
+				},
+			},
+		],
+	},
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
+	},
+	networks: {
+		hardhat: {
+			allowUnlimitedContractSize: true,
+			blockGasLimit: 0x1fffffffffffff,
+		},
+	},
 };
